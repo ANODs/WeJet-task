ebf0f64ded8f91fa645b3319fd0a127224e39de9
Tim Timoshkin <51094534+ANODs@users.noreply.github.com>
2022-09-15 21:42:07 +0300

    ~30% done

---
+++ website/src/scripts/spacemanParalax.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    console.clear();
+
+    $('#header').on('mousemove',(e)=>{e.preventDefault()
+        let spaceman = document.querySelector('.spaceman > img');
+        let movementValue = spaceman.getAttribute('data-parallax');
+
+        let dx = e.clientX * movementValue;
+        let dy = e.clientY * movementValue;
+        let w = window.innerWidth;
+        let h = window.innerHeight;
+        w>h ? dx = dx*(h/w) : dy = dy*(w/h);
+
+            spaceman.style.transform = "translate("+-dx+"px,"+-dy+"px)";
+        })
+});
